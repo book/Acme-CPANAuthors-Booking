ee42b8a323ff2a7f362f3bef94115f2b1e9bf287
Abigail <abigail@abigail.be>
2010-09-15 02:55:23 +0200

    Mention requirements in Makefile.PL

--- lib/Acme/CPANAuthors/Booking.pm
+++ lib/Acme/CPANAuthors/Booking.pm
@@ -5,7 +5,7 @@ use strict;
 use warnings;
 no  warnings 'syntax';
 
-our $VERSION = '2010091101';
+our $VERSION = '2010091501';
 
 use Acme::CPANAuthors::Register (
     ABIGAIL       =>   "Abigail",
