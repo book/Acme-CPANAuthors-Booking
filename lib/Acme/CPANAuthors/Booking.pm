1a3adfd9df318477dd69493f5590dad168af6a1d
Abigail <abigail@abigail.be>
2012-05-25 22:38:04 +0200

    New version

--- lib/Acme/CPANAuthors/Booking.pm
+++ lib/Acme/CPANAuthors/Booking.pm
@@ -5,7 +5,7 @@ use strict;
 use warnings;
 no  warnings 'syntax';
 
-our $VERSION = '2012050801';
+our $VERSION = '2012052501';
 
 use Acme::CPANAuthors::Register (
     ABIGAIL       =>   "Abigail",
