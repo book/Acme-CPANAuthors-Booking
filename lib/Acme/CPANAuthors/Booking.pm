7b51ba792e13aeb4ec30edaa1d0434c74d01630b
Philippe Bruhat (BooK) <book@cpan.org>
2014-04-05 23:21:53 +0200

    Changes for version 2014040501

--- lib/Acme/CPANAuthors/Booking.pm
+++ lib/Acme/CPANAuthors/Booking.pm
@@ -5,7 +5,7 @@ use strict;
 use warnings;
 no  warnings 'syntax';
 
-our $VERSION = '2014032801';
+our $VERSION = '2014040501';
 
 use Acme::CPANAuthors::Register (
     ABCDEFGH      =>   "poddar (Amit Poddar)",
